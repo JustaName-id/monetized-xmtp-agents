7f0bddf8de8ca6a7f39a231eb2aa2c0850e59f10
Ghadi Mhawej <ghadimhawej@outlook.com>
2025-06-04 11:14:57 +0300

    feat: updated tx reference link + add base address to subnames

--- apps/hub/src/components/MessageCard.tsx
+++ apps/hub/src/components/MessageCard.tsx
@@ -10,6 +10,7 @@ import { EncodedContent } from '@xmtp/content-type-primitives';
 import { ContentTypeText } from '@xmtp/content-type-text';
 import { ContentTypeGroupUpdated } from '@xmtp/content-type-group-updated';
 import Link from 'next/link';
+import { clientEnv } from '@/utils/config/clientEnv';
 
 export interface MessageCardProps {
   message: DecodedMessage;
@@ -20,8 +21,6 @@ export const MessageCard: React.FC<MessageCardProps> = ({
   message,
   isSender,
 }) => {
-
-
   if (message.contentType.sameAs(ContentTypeText)) {
     return (
       <div
@@ -57,7 +56,7 @@ export const MessageCard: React.FC<MessageCardProps> = ({
       <Link
         className={'underline underline-offset-2 text-white'}
         target={'_blank'}
-        href={`https://sepolia.basescan.org/tx/${tx.reference}`}
+        href={`${clientEnv.baseNetwork.blockExplorers?.default.url}/tx/${tx.reference}`}
       >
         <div
           className={`flex flex-row w-full ${
@@ -78,9 +77,8 @@ export const MessageCard: React.FC<MessageCardProps> = ({
     );
   }
 
-
   if (message.contentType.sameAs(ContentTypeGroupUpdated)) {
-    return null
+    return null;
   }
 
   if (typeof message.fallback === 'string') {
